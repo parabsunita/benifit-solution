463214abc4754e67d0bc32083786dfa8cc57a810
Your Name <parabsunita10397@gmail.com>
2024-12-25 19:46:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FlexibleBenefits from './FlexibleBenefits';
+
+function App() {
+  return (
+    <div className="App">
+     <FlexibleBenefits />
+    </div>
+  );
+}
+
+export default App;
